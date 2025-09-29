3cb1460b40fc02168e682f7e4305b6250ea341e8
Gabriel <fcp13tv@gmail.com>
2025-09-29 15:56:44 +0200

    script npm start mise a jour

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('./node_modules/express');
-const path = require('./node_modules/path');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
